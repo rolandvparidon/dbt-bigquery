ed2fd6cc07063a4cfee489c3521b98fa0cabee4d
Roland <kana.stumble-0z@icloud.com>
2025-02-27 20:04:59 +0000

    adding window functions

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -77,10 +77,13 @@ final as (
 
         -- new vs returning customer
         case 
-            when customer_orders.first_order_date = paid_orders.order_placed_at
-            then 'new'
-            else 'return' 
-        end as nvsr,
+            when (
+                rank() over (
+                    partition by customer_id
+                    order by order_placed_at, order_id
+                    ) = 1
+            ) then 'new'
+        else 'return' end as nvsr,
 
         -- customer lifetime value
         sum(total_amount_paid) over (
@@ -89,7 +92,11 @@ final as (
         ) as customer_lifetime_value,
         
         -- first day of sale
-        customer_orders.first_order_date as fdos
+        first_value(paid_orders.orders_placed_at) over (
+            partition by paid_orders.customer_id
+            order by paid_orders.order_placed_at
+        ) as fdos
+        
         from paid_orders
         left join customer_orders on paid_orders.customer_id = customer_orders.customer_id
         order by order_id
