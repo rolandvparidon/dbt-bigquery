b9308179d2bd4b7be5d2fb12badfa3b618aa3d0e
Roland <kana.stumble-0z@icloud.com>
2025-02-28 23:00:55 +0000

    cents_to_dollar macro

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -15,7 +15,7 @@ renamed as (
         orderid as order_id,
         paymentmethod as payment_method,
         status,
-        amount,
+        {{cents_to_dollars('amount', 4)}} as amount,
         created as created_at,
     from source
 
