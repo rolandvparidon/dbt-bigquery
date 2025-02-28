9b40a9c7a0dec295ee43852e6523249d1655c7e0
Roland <kana.stumble-0z@icloud.com>
2025-02-28 22:43:07 +0000

    jinja for loop and if statement

--- models/marts/core/int_orders__pivoted.sql
+++ models/marts/core/int_orders__pivoted.sql
@@ -3,7 +3,25 @@ with payments as (
 ),
 
 pivoted as (
-    select * from payments
+    select 
+        order_id,
+
+        {% set payment_methods = ['bank_transfer', 'coupon', 'credit_card', 'gift_card'] %}
+
+        {% for payment_method in payment_methods %}
+
+        sum(case when payment_method = '{{ payment_method }}' then amount else 0 end) as {{ payment_method }}_transfer_amount
+        {% if not loop.last %}
+            ,
+        {% endif %}
+        
+        {% endfor %}
+        
+        
+    
+    from payments
+    where status = 'success'
+    group by 1
 )
 
 select * from pivoted
